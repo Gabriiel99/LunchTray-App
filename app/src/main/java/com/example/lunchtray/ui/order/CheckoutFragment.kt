bb3d96da43614fa341628be04038c8a25a7bf3af
Gabriel <gabriel99luques@gmail.com>
2022-05-12 17:58:13 -0300

    Reset order in view model

--- app/src/main/java/com/example/lunchtray/ui/order/CheckoutFragment.kt
+++ app/src/main/java/com/example/lunchtray/ui/order/CheckoutFragment.kt
@@ -81,7 +81,8 @@ class CheckoutFragment : Fragment() {
     fun submitOrder() {
         // Show snackbar to "confirm" order
         Snackbar.make(binding.root, R.string.submit_order, Snackbar.LENGTH_SHORT).show()
-        // TODO: Reset order in view model
+
+        sharedViewModel.resetOrder()
         // TODO: Navigate back to the [StartFragment] to start over
     }
 
